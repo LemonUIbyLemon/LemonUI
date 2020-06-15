8b4008f0210a18436aa2e9b923eade4cc513e921
Hannele Ruiz <justlemoncl@gmail.com>
2020-06-15 18:00:06 -0400

    Added interfaces for the items

---
+++ LemonUI/Items/IItem.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace LemonUI.Items
+{
+    /// <summary>
+    /// A standard menu item.
+    /// </summary>
+    public interface IItem
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The title of this item.
+        /// </summary>
+        string Title { get; set; }
+        /// <summary>
+        /// The description of the item.
+        /// </summary>
+        string Description { get; set; }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Event triggered when the item is selected.
+        /// </summary>
+        event EventHandler Selected;
+
+        #endregion
+    }
+}
---
+++ LemonUI/Items/IItemSlidable.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace LemonUI.Items
+{
+    /// <summary>
+    /// A menu item that can go Left and Right.
+    /// </summary>
+    public interface IItemSlidable : IItem
+    {
+        #region Public Functions
+
+        /// <summary>
+        /// Moves to the Left.
+        /// </summary>
+        void GoLeft();
+        /// <summary>
+        /// Moves to the Right.
+        /// </summary>
+        void GoRight();
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Event triggered when the item has gone to the left.
+        /// </summary>
+        event EventHandler Left;
+        /// <summary>
+        /// Event triggered when the item has gone to the right.
+        /// </summary>
+        event EventHandler Right;
+
+        #endregion
+    }
+}
