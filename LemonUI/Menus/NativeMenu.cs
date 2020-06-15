2548bf8f2ef6d3219c70ae30cbfccaf3038f2d41
Hannele Ruiz <justlemoncl@gmail.com>
2020-06-15 19:26:15 -0400

    Added basics for the Native Rockstar menu

---
+++ LemonUI/Menus/NativeMenu.cs
@@ -0,0 +1,79 @@
+using LemonUI.Elements;
+using LemonUI.Items;
+using LemonUI.Menus;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace LemonUI.NativeMenu
+{
+    /// <summary>
+    /// Menu that looks like the ones used by Rockstar.
+    /// </summary>
+    public class NativeMenu : INativeMenu
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The title of the menu.
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// The current index of the menu.
+        /// </summary>
+        public int Index { get; set; }
+        /// <summary>
+        /// The items that this menu contain.
+        /// </summary>
+        public List<IItem> Items { get; }
+        /// <summary>
+        /// The banner shown at the top of the menu.
+        /// </summary>
+        public IDrawable Banner { get; set; }
+        /// <summary>
+        /// Text shown when there are no items in the menu.
+        /// </summary>
+        public string NoItemsText { get; set; }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Event triggered when the menu is opened and shown to the user.
+        /// </summary>
+        public event EventHandler Shown;
+        /// <summary>
+        /// Event triggered when the menu starts closing.
+        /// </summary>
+        public event CancelEventHandler Closing;
+        /// <summary>
+        /// Event triggered when the menu finishes closing.
+        /// </summary>
+        public event EventHandler Closed;
+        /// <summary>
+        /// Event triggered when an item is selected.
+        /// </summary>
+        public event EventHandler Selected;
+        /// <summary>
+        /// Event triggered when the selected index has been changed.
+        /// </summary>
+        public event EventHandler ItemChanged;
+        /// <summary>
+        /// Event triggered when the index has been changed.
+        /// </summary>
+        public event EventHandler IndexChanged;
+
+        #endregion
+
+        #region Constructor
+
+        public NativeMenu(string title)
+        {
+            // Just save the title
+            Title = title;
+        }
+
+        #endregion
+    }
+}
