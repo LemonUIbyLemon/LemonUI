4467868b961c8f61b6795afbc76d0925febf0fd8
Hannele Ruiz <justlemoncl@gmail.com>
2020-06-15 15:11:30 -0400

    Added class to draw rectangles on the screen

---
+++ LemonUI/Elements/ScaledRectangle.cs
@@ -0,0 +1,40 @@
+#if FIVEM
+using CitizenFX.Core.Native;
+#else
+using GTA.Native;
+#endif
+using System.Drawing;
+
+namespace LemonUI
+{
+    /// <summary>
+    /// A 2D rectangle.
+    /// </summary>
+    public class ScaledRectangle : BaseElement
+    {
+        #region Constructor
+
+        public ScaledRectangle(PointF pos, SizeF size) : base(pos, size)
+        {
+        }
+
+        #endregion
+
+        #region Public Functions
+
+        /// <summary>
+        /// Draws the rectangle on the screen.
+        /// </summary>
+        public override void Draw()
+        {
+#if FIVEM
+            API.DrawRect(relativePosition.X, relativePosition.Y, relativeSize.Width, relativeSize.Height, Color.R, Color.G, Color.B, Color.A);
+#else
+
+            Function.Call(Hash.DRAW_RECT, relativePosition.X, relativePosition.Y, relativeSize.Width, relativeSize.Height, Color.R, Color.G, Color.B, Color.A);
+#endif
+        }
+
+        #endregion
+    }
+}
