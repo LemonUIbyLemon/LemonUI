0abdfc74cb2a1467a7717bac3bb88b76094f612c
Hannele Ruiz <justlemoncl@gmail.com>
2020-06-15 14:12:18 -0400

    Added interface for 2D items on the screen

---
+++ LemonUI/Elements/IDrawable.cs
@@ -0,0 +1,40 @@
+using System.Drawing;
+
+namespace LemonUI
+{
+    /// <summary>
+    /// A 2D item that can be drawn on the screen.
+    /// </summary>
+    public interface IDrawable
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The Position of the drawable.
+        /// </summary>
+        PointF Position { get; set; }
+        /// <summary>
+        /// The Size of the drawable.
+        /// </summary>
+        SizeF Size { get; set; }
+        /// <summary>
+        /// The Color of the drawable.
+        /// </summary>
+        Color Color { get; set; }
+        /// <summary>
+        /// The rotation of the drawable.
+        /// </summary>
+        float Heading { get; set; }
+
+        #endregion
+
+        #region Public Functions
+
+        /// <summary>
+        /// Draws the item on the screen.
+        /// </summary>
+        void Draw();
+
+        #endregion
+    }
+}
