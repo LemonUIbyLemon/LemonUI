0c71102448fd0e76aa64897be4abcc168bf7c8f9
Hannele Ruiz <justlemoncl@gmail.com>
2020-06-21 23:33:36 -0400

    Moved text processing into separate function

--- LemonUI/Elements/ScaledText.cs
+++ LemonUI/Elements/ScaledText.cs
@@ -153,6 +153,75 @@ namespace LemonUI.Elements
         #region Tools
 
         /// <summary>
+        /// Adds the text information for measurement.
+        /// </summary>
+        private void Add()
+        {
+#if FIVEM
+            API.SetTextFont((int)Font);
+            API.SetTextScale(1f, Scale);
+            API.SetTextColour(Color.R, Color.G, Color.B, Color.A);
+            if (Shadow)
+            {
+                API.SetTextDropShadow();
+            }
+            if (Outline)
+            {
+                API.SetTextOutline();
+            }
+            switch (Alignment)
+            {
+                case Alignment.Center:
+                    API.SetTextCentre(true);
+                    break;
+                case Alignment.Right:
+                    API.SetTextRightJustify(true);
+                    API.SetTextWrap(0f, relativePosition.X);
+                    break;
+            }
+            if (WordWrap > 0)
+            {
+                API.SetTextWrap(relativePosition.X, realWrap);
+            }
+            API.SetTextEntry("CELL_EMAIL_BCON");
+            foreach (string chunk in chunks)
+            {
+                API.AddTextComponentString(chunk);
+            }
+#else
+            Function.Call(Hash.SET_TEXT_FONT, (int)Font);
+            Function.Call(Hash.SET_TEXT_SCALE, 1f, Scale);
+            Function.Call(Hash.SET_TEXT_COLOUR, Color.R, Color.G, Color.B, Color.A);
+            if (Shadow)
+            {
+                Function.Call(Hash.SET_TEXT_DROP_SHADOW);
+            }
+            if (Outline)
+            {
+                Function.Call(Hash.SET_TEXT_OUTLINE);
+            }
+            switch (Alignment)
+            {
+                case Alignment.Center:
+                    Function.Call(Hash.SET_TEXT_CENTRE, true);
+                    break;
+                case Alignment.Right:
+                    Function.Call(Hash.SET_TEXT_RIGHT_JUSTIFY, true);
+                    Function.Call(Hash.SET_TEXT_WRAP, 0f, relativePosition.X);
+                    break;
+            }
+            if (WordWrap > 0)
+            {
+                Function.Call(Hash.SET_TEXT_WRAP, relativePosition.X, realWrap);
+            }
+            Function.Call((Hash)0x25FBB336DF1804CB, "CELL_EMAIL_BCON"); // _SET_TEXT_ENTRY on v2, BEGIN_TEXT_COMMAND_DISPLAY_TEXT on v3
+            foreach (string chunk in chunks)
+            {
+                Function.Call((Hash)0x6C188BE134E074AA, chunk); // _ADD_TEXT_COMPONENT_STRING on v2, ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME on v3
+            }
+#endif
+        }
+        /// <summary>
         /// Slices the string of text into appropiately saved chunks.
         /// </summary>
         private void Slice()
@@ -230,69 +299,10 @@ namespace LemonUI.Elements
         /// </summary>
         public override void Draw()
         {
+            Add();
 #if FIVEM
-            API.SetTextFont((int)Font);
-            API.SetTextScale(1f, Scale);
-            API.SetTextColour(Color.R, Color.G, Color.B, Color.A);
-            if (Shadow)
-            {
-                API.SetTextDropShadow();
-            }
-            if (Outline)
-            {
-                API.SetTextOutline();
-            }
-            switch (Alignment)
-            {
-                case Alignment.Center:
-                    API.SetTextCentre(true);
-                    break;
-                case Alignment.Right:
-                    API.SetTextRightJustify(true);
-                    API.SetTextWrap(0f, relativePosition.X);
-                    break;
-            }
-            if (WordWrap > 0)
-            {
-                API.SetTextWrap(relativePosition.X, realWrap);
-            }
-            API.SetTextEntry("CELL_EMAIL_BCON");
-            foreach (string chunk in chunks)
-            {
-                API.AddTextComponentString(chunk);
-            }
             API.DrawText(relativePosition.X, relativePosition.Y);
 #else
-            Function.Call(Hash.SET_TEXT_FONT, (int)Font);
-            Function.Call(Hash.SET_TEXT_SCALE, 1f, Scale);
-            Function.Call(Hash.SET_TEXT_COLOUR, Color.R, Color.G, Color.B, Color.A);
-            if (Shadow)
-            {
-                Function.Call(Hash.SET_TEXT_DROP_SHADOW);
-            }
-            if (Outline)
-            {
-                Function.Call(Hash.SET_TEXT_OUTLINE);
-            }
-            switch (Alignment)
-            {
-                case Alignment.Center:
-                    Function.Call(Hash.SET_TEXT_CENTRE, true);
-                    break;
-                case Alignment.Right:
-                    Function.Call(Hash.SET_TEXT_RIGHT_JUSTIFY, true);
-                    Function.Call(Hash.SET_TEXT_WRAP, 0f, relativePosition.X);
-                    break;
-            }
-            if (WordWrap > 0)
-            {
-                Function.Call(Hash.SET_TEXT_WRAP, relativePosition.X, realWrap);
-            }
-            Function.Call((Hash)0x25FBB336DF1804CB, "CELL_EMAIL_BCON"); // _SET_TEXT_ENTRY on v2, BEGIN_TEXT_COMMAND_DISPLAY_TEXT on v3
-            foreach (string chunk in chunks)
-            {
-                Function.Call((Hash)0x6C188BE134E074AA, chunk); // _ADD_TEXT_COMPONENT_STRING on v2, ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME on v3
-            }
             Function.Call((Hash)0xCD015E5BB0D96A57, relativePosition.X, relativePosition.Y); // _DRAW_TEXT on v2, END_TEXT_COMMAND_DISPLAY_TEXT on v3
 #endif
         }
