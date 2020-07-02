d913851242902e9045856b17c90d6e25ecf16d47
Hannele Ruiz <justlemoncl@gmail.com>
2020-07-02 10:36:25 -0400

    Added internal functions to check if the cursor is in specific bounds

--- LemonUI/Extensions/Resolution.cs
+++ LemonUI/Extensions/Resolution.cs
@@ -1,8 +1,11 @@
 #if FIVEM
+using CitizenFX.Core;
 using CitizenFX.Core.Native;
 #else
+using GTA;
 using GTA.Native;
 #endif
+using System.Drawing;
 
 namespace LemonUI.Extensions
 {
@@ -56,6 +59,51 @@ namespace LemonUI.Extensions
             // And save the correct values
             xout = xin / width;
             yout = yin / 1080f;
+        }
+        /// <summary>
+        /// Checks if the cursor is at the specified location.
+        /// </summary>
+        /// <param name="pos">The position of the area.</param>
+        /// <param name="size">The size of the area to check.</param>
+        /// <returns></returns>
+        public static bool IsCursorInBounds(PointF pos, SizeF size) => IsCursorInBounds(pos.X, pos.Y, size.Width, size.Height);
+        /// <summary>
+        /// Checks if the cursor is at the specified location.
+        /// </summary>
+        /// <param name="x">The absolute X position.</param>
+        /// <param name="y">The absolute Y position.</param>
+        /// <param name="width">The absolute Width from X.</param>
+        /// <param name="height">The absolute Height from Y.</param>
+        /// <returns>True if the mouse is in the specified location, False otherwise.</returns>
+        public static bool IsCursorInBounds(float x, float y, float width, float height)
+        {
+            // Get the location where the cursor is
+#if FIVEM
+            float cursorX = API.GetControlNormal(0, (int)Control.CursorX);
+            float cursorY = API.GetControlNormal(0, (int)Control.CursorY);
+#else
+            float cursorX = Function.Call<float>(Hash.GET_CONTROL_NORMAL, 0, (int)Control.CursorX);
+            float cursorY = Function.Call<float>(Hash.GET_CONTROL_NORMAL, 0, (int)Control.CursorY);
+#endif
+            // Convert the values received to relative
+            ToRelative(x, y, out float startX, out float startY);
+            ToRelative(width, height, out float realWidth, out float realHeight);
+            // And get the correct positions of them
+            float realX = 0, realY = 0;
+#if FIVEM
+            API.GetScriptGfxPosition(startX, startY, ref realX, ref realY);
+#else
+            OutputArgument argX = new OutputArgument();
+            OutputArgument argY = new OutputArgument();
+            Function.Call((Hash)0x6DD8F5AA635EB4B2, startX, startY, argX, argY);
+            realX = argX.GetResult<float>();
+            realY = argY.GetResult<float>();
+#endif
+            // Check if the values are in the correct positions
+            bool isX = cursorX >= realX && cursorX <= realX + realWidth;
+            bool isY = cursorY > realY && cursorY < realY + realHeight;
+            // And return the result of the checks
+            return isX && isY;
         }
     }
 }
