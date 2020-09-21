d1d582894d785a59fcb2a57bf693dfad5615db1c
Drew Ridley <drew.ridley03@gmail.com>
2020-09-20 23:01:22 -0400

    Created new NativeSeparatorItem

---
+++ LemonUI/Menus/NativeSeparatorItem.cs
@@ -0,0 +1,15 @@
+﻿namespace LemonUI.Menus
+{
+    /// <summary>
+    /// A blank separator item for dividing menus.
+    /// </summary>
+    public class NativeSeparatorItem: NativeItem
+    {
+        /// <summary>
+        /// Creates a new NativeSeparator
+        /// </summary>
+        public NativeSeparatorItem() : base("", "", "")
+        {
+        }
+    }
+}
