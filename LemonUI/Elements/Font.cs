3ccb40adc23cfb213490ab783080598831a78a73
Hannele Ruiz <justlemoncl@gmail.com>
2021-04-12 00:05:50 -0400

    Added Font enum for RPH

---
+++ LemonUI/Elements/Font.cs
@@ -0,0 +1,31 @@
+#if RPH
+namespace LemonUI.Elements
+{
+    /// <summary>
+    /// An enum representing the fonts available in game.
+    /// </summary>
+    public enum Font
+    {
+        /// <summary>
+        /// Chalet London Nineteen Sixty.
+        /// </summary>
+        ChaletLondon = 0,
+        /// <summary>
+        /// SignPainter HouseScript Regular.
+        /// </summary>
+        HouseScript = 1,
+        /// <summary>
+        /// Unknown Monospaced Font.
+        /// </summary>
+        Monospace = 2,
+        /// <summary>
+        /// Chalet Comprime Cologne.
+        /// </summary>
+        ChaletComprimeCologne = 4,
+        /// <summary>
+        /// Pricedown.
+        /// </summary>
+        Pricedown = 7
+    }
+}
+#endif
