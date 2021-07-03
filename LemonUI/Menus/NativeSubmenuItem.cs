4cf7babe7f4ca5e9c4fd1bed6f620415e9bb6976
CENSOR_1337 <viixxivvimm@gmail.com>
2021-07-03 13:36:44 +0700

    Added constructor to NativeSubmenuItem
    - Added constructor to `NativeSubmenuItem` that allow to use custom string instend of `">>>"`.

--- LemonUI/Menus/NativeSubmenuItem.cs
+++ LemonUI/Menus/NativeSubmenuItem.cs
@@ -29,6 +29,18 @@ namespace LemonUI.Menus
             Menu.Parent = parent ?? throw new ArgumentNullException(nameof(parent));
             Activated += NativeSubmenuItem_Activated;
         }
+        /// <summary>
+        /// Creates a new Item that opens a Submenu.
+        /// </summary>
+        /// <param name="menu">The menu that this item will open.</param>
+        /// <param name="parent">The parent menu where this item will be located.</param>
+        /// <param name="endlabel">The string will be displayed at the end of submenu.</param>
+        public NativeSubmenuItem(NativeMenu menu, NativeMenu parent, String endlabel) : base(menu.Subtitle, menu.Description, endlabel)
+        {
+            Menu = menu ?? throw new ArgumentNullException(nameof(menu));
+            Menu.Parent = parent ?? throw new ArgumentNullException(nameof(parent));
+            Activated += NativeSubmenuItem_Activated;
+        }
 
         #endregion
 
