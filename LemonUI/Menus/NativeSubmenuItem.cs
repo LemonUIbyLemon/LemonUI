1fea303f1465440796ce61f85213d3fbd98aa3f2
CENSOR_1337 <viixxivvimm@gmail.com>
2021-07-05 20:41:47 +0700

    Fixed section

--- LemonUI/Menus/NativeSubmenuItem.cs
+++ LemonUI/Menus/NativeSubmenuItem.cs
@@ -24,6 +24,10 @@ namespace LemonUI.Menus
         /// <param name="parent">The parent menu where this item will be located.</param>
         public NativeSubmenuItem(NativeMenu menu, NativeMenu parent) : base(menu.Subtitle, menu.Description, ">>>")
         {
+            Menu = menu ?? throw new ArgumentNullException(nameof(menu));
+            Menu.Parent = parent ?? throw new ArgumentNullException(nameof(parent));
+            Activated += NativeSubmenuItem_Activated;
+
         }
         /// <summary>
         /// Creates a new Item that opens a Submenu.
@@ -31,13 +35,9 @@ namespace LemonUI.Menus
         /// <param name="menu">The menu that this item will open.</param>
         /// <param name="parent">The parent menu where this item will be located.</param>
         /// <param name="endlabel">The alternative title of the item, shown on the right.</param>
-        public NativeSubmenuItem(NativeMenu menu, NativeMenu parent, string endlabel)
+        public NativeSubmenuItem(NativeMenu menu, NativeMenu parent, string endlabel) : this(menu, parent)
         {
-            Menu = menu ?? throw new ArgumentNullException(nameof(menu));
-            Menu.Parent = parent ?? throw new ArgumentNullException(nameof(parent));
             altTitle.Text = endlabel;
-
-            Activated += NativeSubmenuItem_Activated;
         }
 
         #endregion
