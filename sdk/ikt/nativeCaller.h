ffe2dfe9faa767d7e58f0304706a16474385fab8
Hannele Ruiz <justlemoncl@gmail.com>
2021-09-07 00:54:59 -0300

    Added ScriptHookV download on build

---
+++ sdk/ikt/nativeCaller.h
@@ -0,0 +1,32 @@
+// https://github.com/E66666666/GTAVManualTransmission/blob/master/thirdparty/ScriptHookV_SDK/inc/nativeCaller.h
+
+#pragma once
+
+#include "main.h"
+#include <utility>
+
+template <typename T>
+static inline void nativePush(T val)
+{
+    static_assert(sizeof(T) <= sizeof(UINT64), "Type is too large");
+    UINT64 val64 = 0;
+    *reinterpret_cast<T *>(&val64) = val;
+    nativePush64(val64);
+}
+
+template <typename R>
+static inline R invoke(UINT64 hash)
+{
+    nativeInit(hash);
+    return *reinterpret_cast<R *>(nativeCall());
+}
+
+template <typename R, class ... Args>
+static inline R invoke(UINT64 hash, Args&& ... args)
+{
+    nativeInit(hash);
+
+    (nativePush(std::forward<Args>(args)), ...);
+
+    return *reinterpret_cast<R*>(nativeCall());
+}
