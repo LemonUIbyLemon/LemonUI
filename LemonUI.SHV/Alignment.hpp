b0a6c0f727c580baaf1b4ddf44f59259ae31d1fd
Hannele Ruiz <justlemoncl@gmail.com>
2021-09-07 23:37:01 -0300

    Added the Alignment enum to C++

---
+++ LemonUI.SHV/Alignment.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace LemonUI
+{
+	/// <summary>
+	/// The alignment of the element to draw.
+	/// </summary>
+	enum Alignment
+	{
+		/// <summary>
+		/// Aligns the element to the Center.
+		/// </summary>
+		Center = 0,
+		/// <summary>
+		/// Aligns the element to the Left.
+		/// </summary>
+		Left = 1,
+		/// <summary>
+		/// Aligns the element to the RIght.
+		/// </summary>
+		Right = 2,
+	};
+}
