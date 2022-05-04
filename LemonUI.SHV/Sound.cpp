b4f4dcca049611a7b744b5bf0e82705f0f45f446
EntenKoeniq <81123713+EntenKoeniq@users.noreply.github.com>
2022-05-04 02:59:38 +0200

    Started with LemonUI.SHV

---
+++ LemonUI.SHV/Sound.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "Sound.h"
+
+#include <natives.h>
+
+namespace LemonUI
+{
+	Sound::Sound(const std::string& sound, const std::string& set)
+	{
+		this->_soundName = std::make_unique<std::string>(sound);
+		this->_setName = std::make_unique<std::string>(set);
+	}
+	Sound::Sound()
+	{}
+
+	void Sound::playFrontend() const
+	{
+		std::string soundName = *this->_soundName.get();
+		std::string setName = *this->_setName.get();
+		
+		AUDIO::PLAY_SOUND_FRONTEND(-1, const_cast<char*>(soundName.c_str()), const_cast<char*>(setName.c_str()), false);
+		int id = AUDIO::GET_SOUND_ID();
+		AUDIO::RELEASE_SOUND_ID(id);
+	}
+}
---
+++ LemonUI.SHV/pch.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN // Selten verwendete Komponenten aus Windows-Headern ausschließen
+
+#include <string>
+#include <memory>
