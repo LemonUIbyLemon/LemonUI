8adb1a44427c19f37defad5d1a793dcd705bb4d0
Hannele Ruiz <justlemoncl@gmail.com>
2022-07-16 18:33:18 -0400

    Added event arguments and handler for contact changes

---
+++ LemonUI/Phone/PhoneContactChangedEventHandler.cs
@@ -0,0 +1,11 @@
+﻿#if SHVDN3
+namespace LemonUI.Phone
+{
+    /// <summary>
+    /// Represents the method that is called when one of the properties of the contact are changed.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">An <see cref="PhoneContactChangedEventArgs"/> with the contact information.</param>
+    public delegate void PhoneContactChangedEventHandler(object sender, PhoneContactChangedEventArgs e);
+}
+#endif
