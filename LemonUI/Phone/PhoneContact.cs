f4aedc659d9b59d3cca914af19028fd4afa87890
Hannele Ruiz <justlemoncl@gmail.com>
2022-07-16 17:17:47 -0400

    Added basic Phone Contact class

---
+++ LemonUI/Phone/PhoneContact.cs
@@ -0,0 +1,36 @@
+﻿#if SHVDN3
+using System;
+
+namespace LemonUI.Phone
+{
+    /// <summary>
+    /// A phone contact.
+    /// </summary>
+    public class PhoneContact
+    {
+        #region Events
+
+        /// <summary>
+        /// Event triggered when the contact is called.
+        /// </summary>
+        public event EventHandler Called;
+        /// <summary>
+        /// Event triggered when the call is answered by the player.
+        /// </summary>
+        /// <remarks>
+        /// This event will trigger both, when the player calls the contact and when the contact calls the player.
+        /// </remarks>
+        public event EventHandler Answered;
+        /// <summary>
+        /// Event triggered when the call is hung up by the player.
+        /// </summary>
+        public event EventHandler Cancelled;
+        /// <summary>
+        /// Event triggered when the call finishes naturally, without player input.
+        /// </summary>
+        public event EventHandler Finished;
+
+        #endregion
+    }
+}
+#endif
