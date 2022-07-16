433ea3b15163155a907f274f0b8d3663b45612f2
Hannele Ruiz <justlemoncl@gmail.com>
2022-07-16 17:28:17 -0400

    Added function to add and remove contacts

--- LemonUI/Phone/Phone.cs
+++ LemonUI/Phone/Phone.cs
@@ -1,4 +1,6 @@
 #if SHVDN3
+using System.Collections.Generic;
+
 namespace LemonUI.Phone
 {
     /// <summary>
@@ -6,6 +8,35 @@ namespace LemonUI.Phone
     /// </summary>
     public static class Phone
     {
+        #region Fields
+
+        private static readonly List<PhoneContact> contacts = new List<PhoneContact>();
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// Adds a new contact to the phone.
+        /// </summary>
+        /// <param name="contact">The contact to add.</param>
+        public static void Add(PhoneContact contact)
+        {
+            if (contacts.Contains(contact))
+            {
+                return;
+            }
+
+            contacts.Add(contact);
+        }
+        /// <summary>
+        /// Removes a contact from the phone.
+        /// </summary>
+        /// <param name="contact">The contact to remove.</param>
+        /// <returns><see langword="true"/> if the contact was removed, <see langword="false"/> otherwise.</returns>
+        public static bool Remove(PhoneContact contact) => contacts.Remove(contact);
+
+        #endregion
     }
 }
 #endif
