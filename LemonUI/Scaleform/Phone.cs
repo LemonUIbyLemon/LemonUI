2f3c30f248307470c730439bd8b99cc9c653ebd3
Hannele Ruiz <justlemoncl@gmail.com>
2022-07-16 22:46:41 -0400

    Added Scaleform class for handling phones

---
+++ LemonUI/Scaleform/Phone.cs
@@ -0,0 +1,50 @@
+﻿namespace LemonUI.Scaleform
+{
+    /// <summary>
+    /// Class used to manage the phone scaleform.
+    /// </summary>
+    public sealed class Phone : BaseScaleform
+    {
+        #region Properties
+
+        /// <summary>
+        /// The type of phone.
+        /// </summary>
+        public PhoneType Type { get; }
+
+        #endregion
+        
+        #region Constructors
+
+        /// <inheritdoc/>
+        public Phone(PhoneType type) : base(GetName(type))
+        {
+            Type = type;
+        }
+
+        #endregion
+
+        #region Functions
+
+        private static string GetName(PhoneType type)
+        {
+            switch (type)
+            {
+                case PhoneType.Badger:
+                    return "cellphone_badger";
+                case PhoneType.Facade:
+                    return "cellphone_facade";
+                case PhoneType.IFruit:
+                    return "cellphone_ifruit";
+                default:
+                    return "cellphone_ifruit";
+            }
+        }
+        /// <inheritdoc/>
+        public override void Update()
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ LemonUI/Scaleform/PhoneType.cs
@@ -0,0 +1,21 @@
+﻿namespace LemonUI.Scaleform
+{
+    /// <summary>
+    /// The different type of phones in GTA V.
+    /// </summary>
+    public enum PhoneType
+    {
+        /// <summary>
+        /// Badger, the phone used by Franklin.
+        /// </summary>
+        Badger = 0,
+        /// <summary>
+        /// Facade, the phone used by Trevor.
+        /// </summary>
+        Facade = 1,
+        /// <summary>
+        /// iFruit, the phone used by Michael and the GTA Online protagonist.
+        /// </summary>
+        IFruit = 2
+    }
+}
