39ee265288c18f537c8511b9c4c07351a987259c
Hannele Ruiz <justlemoncl@gmail.com>
2022-07-25 20:29:09 -0400

    Keep track of the sound ID

--- LemonUI/Sound.cs
+++ LemonUI/Sound.cs
@@ -19,6 +19,10 @@ namespace LemonUI
     public class Sound
     {
         /// <summary>
+        /// The ID of the sound, if is being played.
+        /// </summary>
+        public int Id { get; private set; } = -1;
+        /// <summary>
         /// The Set where the sound is located.
         /// </summary>
         public string Set { get; set; }
@@ -44,22 +48,40 @@ namespace LemonUI
         public void PlayFrontend()
         {
 #if FIVEM
-            API.PlaySoundFrontend(-1, File, Set, false);
-            int id = API.GetSoundId();
-            API.ReleaseSoundId(id);
+            Id = API.GetSoundId();
+            API.PlaySoundFrontend(Id, File, Set, true);
 #elif RAGEMP
-            Invoker.Invoke(Natives.PlaySoundFrontend, -1, File, Set, false);
-            int id = Invoker.Invoke<int>(Natives.GetSoundId);
-            Invoker.Invoke(Natives.ReleaseSoundId, id);
+            Id = Invoker.Invoke<int>(Natives.GetSoundId);
+            Invoker.Invoke(Natives.PlaySoundFrontend, Id, File, Set, true);
 #elif RPH
-            NativeFunction.CallByHash<int>(0x67C540AA08E4A6F5, -1, File, Set, false);
-            int id = NativeFunction.CallByHash<int>(0x430386FE9BF80B45);
-            NativeFunction.CallByHash<int>(0x353FC880830B88FA, id);
+            Id = NativeFunction.CallByHash<int>(0x430386FE9BF80B45);
+            NativeFunction.CallByHash<int>(0x67C540AA08E4A6F5, Id, File, Set, true);
 #elif SHVDN3
-            Function.Call(Hash.PLAY_SOUND_FRONTEND, -1, File, Set, false);
-            int id = Function.Call<int>(Hash.GET_SOUND_ID);
-            Function.Call(Hash.RELEASE_SOUND_ID, id);
+            Id = Function.Call<int>(Hash.GET_SOUND_ID);
+            Function.Call(Hash.PLAY_SOUND_FRONTEND, Id, File, Set, true);
 #endif
+            Release();
+        }
+        /// <summary>
+        /// Releases the Sound ID.
+        /// </summary>
+        public void Release()
+        {
+            if (Id == -1)
+            {
+                return;
+            }
+
+#if FIVEM
+            API.ReleaseSoundId(Id);
+#elif RAGEMP
+            Invoker.Invoke(Natives.ReleaseSoundId, Id);
+#elif RPH
+            NativeFunction.CallByHash<int>(0x353FC880830B88FA, Id);
+#elif SHVDN3
+            Function.Call(Hash.RELEASE_SOUND_ID, Id);
+#endif
+            Id = -1;
         }
     }
 }
