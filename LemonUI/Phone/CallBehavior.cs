97239f224c9443920afbafaf68aacd61ab7894cf
Hannele Ruiz <justlemoncl@gmail.com>
2022-07-25 20:21:58 -0400

    Added enum with the behavior of the call

---
+++ LemonUI/Phone/CallBehavior.cs
@@ -0,0 +1,17 @@
+﻿namespace LemonUI.Phone
+{
+    /// <summary>
+    /// The behavior of a call after is initiated by the user.
+    /// </summary>
+    public enum CallBehavior
+    {
+        /// <summary>
+        /// The contact being called is busy.
+        /// </summary>
+        Busy = 0,
+        /// <summary>
+        /// The contact is available and will answer.
+        /// </summary>
+        Available = 1
+    }
+}
