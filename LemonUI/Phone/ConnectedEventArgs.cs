48139d37cc8017ed00e97e58841f5e23102f7d73
Hannele Ruiz <justlemoncl@gmail.com>
2022-07-26 06:27:48 -0400

    Added event Arguments and Handler for when the call is connected

---
+++ LemonUI/Phone/ConnectedEventArgs.cs
@@ -0,0 +1,30 @@
+﻿#if SHVDN3
+using System;
+
+namespace LemonUI.Phone
+{
+    /// <summary>
+    /// Represents the call process when the player answers the phone and the call is connected.
+    /// </summary>
+    public class ConnectedEventArgs : EventArgs
+    {
+        #region Properties
+
+        /// <summary>
+        /// The contact that was called by the player.
+        /// </summary>
+        public PhoneContact Contact { get; }
+
+        #endregion
+
+        #region Constructors
+
+        internal ConnectedEventArgs(PhoneContact contact)
+        {
+            Contact = contact;
+        }
+
+        #endregion
+    }
+}
+#endif
