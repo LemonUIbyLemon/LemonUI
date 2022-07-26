f8553781779d10ebb78cbc23db40701a52a953be
Hannele Ruiz <justlemoncl@gmail.com>
2022-07-25 20:24:26 -0400

    Added phone contact calling support

--- LemonUI/Phone/PhoneContact.cs
+++ LemonUI/Phone/PhoneContact.cs
@@ -1,4 +1,5 @@
 ﻿#if SHVDN3
+using GTA;
 using System;
 
 namespace LemonUI.Phone
@@ -8,12 +9,19 @@ namespace LemonUI.Phone
     /// </summary>
     public class PhoneContact
     {
+        #region Fields
+
+        private static readonly Sound busySound = new Sound("Phone_SoundSet_Default", "Remote_Engaged");
+        private static readonly Sound callingSound = new Sound("Phone_SoundSet_Default", "Dial_and_Remote_Ring");
+
+        #endregion
+
         #region Events
 
         /// <summary>
-        /// Event triggered when the contact is called.
+        /// Event triggered when the contact is called by the player.
         /// </summary>
-        public event EventHandler Called;
+        public event CalledEventHandler Called;
         /// <summary>
         /// Event triggered when the call is answered by the player.
         /// </summary>
@@ -54,6 +62,58 @@ namespace LemonUI.Phone
         public PhoneContact(string name)
         {
             Name = name;
+        }
+
+        #endregion
+
+        #region Functions
+
+        internal void Call(Scaleform.Phone phone)
+        {
+            const string dialing = "CELL_211";
+            const string busy = "CELL_220";
+            const string connected = "CELL_219";
+
+            callingSound.PlayFrontend(false);
+            phone.ShowCalling(Name, dialing, Icon);
+
+            CalledEventArgs called = new CalledEventArgs(this);
+            Called?.Invoke(phone, called);
+
+            int waitUntil = Game.GameTime + called.Wait;
+
+            while (waitUntil > Game.GameTime)
+            {
+                phone.ShowCalling(Name, dialing, Icon);
+
+                if (Game.IsControlJustPressed(Control.PhoneCancel))
+                {
+                    phone.ShowPage(2);
+                    callingSound.Stop();
+                    return;
+                }
+
+                Script.Yield();
+            }
+
+            callingSound.Stop();
+
+            switch (called.Behavior)
+            {
+                case CallBehavior.Busy:
+                    busySound.PlayFrontend(false);
+                    while (!Game.IsControlPressed(Control.PhoneCancel))
+                    {
+                        phone.ShowCalling(Name, busy, Icon);
+                        Script.Yield();
+                    }
+                    busySound.Stop();
+                    break;
+                case CallBehavior.Available:
+                    phone.ShowCalling(Name, connected, Icon);
+                    Answered?.Invoke(phone, EventArgs.Empty);
+                    break;
+            }
         }
 
         #endregion
