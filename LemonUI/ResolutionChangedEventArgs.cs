0b1586b2e0d72d4957787bdccbddb5a253c485c8
Hannele Ruiz <justlemoncl@gmail.com>
2022-11-09 16:26:16 -0300

    Moved ResolutionChangedEventArgs to it's own file

---
+++ LemonUI/ResolutionChangedEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+namespace LemonUI
+{
+    /// <summary>
+    /// Represents the information after a Resolution Change in the game.
+    /// </summary>
+    public class ResolutionChangedEventArgs
+    {
+        #region Properties
+
+        /// <summary>
+        /// The Game Resolution before it was changed.
+        /// </summary>
+        public SizeF Before { get; }
+        /// <summary>
+        /// The Game Resolution after it was changed.
+        /// </summary>
+        public SizeF After { get; }
+
+        #endregion
+
+        #region Constructors
+
+        internal ResolutionChangedEventArgs(SizeF before, SizeF after)
+        {
+            Before = before;
+            After = after;
+        }
+
+        #endregion
+    }
+}
