655a9f82b16083fc09c4ddbcc49e970eb8008a9f
Hannele Ruiz <justlemoncl@gmail.com>
2022-11-09 16:45:31 -0300

    Added missing parameter documentation in CancelEventHandler

--- LemonUI/CancelEvent.cs
+++ LemonUI/CancelEvent.cs
@@ -10,6 +10,8 @@ namespace LemonUI // Previously System.ComponentModel
     /// <summary>
     /// Represents the method that will handle the event raised when canceling an event.
     /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">A <see cref="CancelEventArgs"/> that contains the event data.</param>
     public delegate void CancelEventHandler(object sender, CancelEventArgs e);
 
     /// <summary>
