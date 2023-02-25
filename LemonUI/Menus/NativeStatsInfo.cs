a008ef01f574840b07a2fe4dd369f0e1907e9036
Hannele Ruiz <justlemoncl@gmail.com>
2023-02-24 21:15:09 -0300

    Renamed Constructor region of NativeStatsInfo

--- LemonUI/Menus/NativeStatsInfo.cs
+++ LemonUI/Menus/NativeStatsInfo.cs
@@ -51,7 +51,7 @@ namespace LemonUI.Menus
 
         #endregion
 
-        #region Constructor
+        #region Constructors
 
         /// <summary>
         /// Creates a new Stat Info with the specified name and value set to zero.
