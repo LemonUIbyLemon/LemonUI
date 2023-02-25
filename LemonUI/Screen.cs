3d9bf98acad911cf280299d9636364e8090ff65e
Hannele Ruiz <justlemoncl@gmail.com>
2023-02-24 21:59:57 -0300

    Moved GFXAlignment to its own file

--- LemonUI/Screen.cs
+++ LemonUI/Screen.cs
@@ -20,33 +20,6 @@ using System.Drawing;
 namespace LemonUI
 {
     /// <summary>
-    /// Represents the internal alignment of screen elements.
-    /// </summary>
-    public enum GFXAlignment
-    {
-        /// <summary>
-        /// Vertical Alignment to the Bottom.
-        /// </summary>
-        Bottom = 66,
-        /// <summary>
-        /// Vertical Alignment to the Top.
-        /// </summary>
-        Top = 84,
-        /// <summary>
-        /// Centered Vertically or Horizontally.
-        /// </summary>
-        Center = 67,
-        /// <summary>
-        /// Horizontal Alignment to the Left.
-        /// </summary>
-        Left = 76,
-        /// <summary>
-        /// Horizontal Alignment to the Right.
-        /// </summary>
-        Right = 82,
-    }
-
-    /// <summary>
     /// Contains a set of tools to work with the screen information.
     /// </summary>
     public static class Screen
