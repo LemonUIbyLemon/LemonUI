475bdf28cb82bf69b1d1d1e797522c3116097955
Hannele Ruiz <justlemoncl@gmail.com>
2023-05-17 19:18:04 -0400

    Added base class for the phone system

---
+++ LemonUI/Phone/PhoneManager.cs
@@ -0,0 +1,28 @@
+namespace LemonUI.Phone
+{
+    /// <summary>
+    /// The manager for the phone contacts.
+    /// </summary>
+    public class PhoneManager : IProcessable
+    {
+        #region Properties
+
+        /// <summary>
+        /// Not used.
+        /// </summary>
+        public bool Visible { get; set; }
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// Processes the phone system.
+        /// </summary>
+        public void Process()
+        {
+        }
+
+        #endregion
+    }
+}
