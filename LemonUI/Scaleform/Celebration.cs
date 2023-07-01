5bc3f0401b293f44edbd69d8da13454bb7af0993
Hannele Ruiz <justlemoncl@gmail.com>
2023-07-01 03:05:34 -0400

    Added background and foreground classes

--- LemonUI/Scaleform/Celebration.cs
+++ LemonUI/Scaleform/Celebration.cs
@@ -5,6 +5,19 @@ namespace LemonUI.Scaleform
     /// </summary>
     public class Celebration : CelebrationCore
     {
+        #region Properties
+
+        /// <summary>
+        /// The background of the scaleform.
+        /// </summary>
+        public CelebrationBackground Background { get; } = new CelebrationBackground();
+        /// <summary>
+        /// The foreground of the scaleform.
+        /// </summary>
+        public CelebrationForeground Foreground { get; } = new CelebrationForeground();
+
+        #endregion
+
         #region Constructors
 
         /// <summary>
