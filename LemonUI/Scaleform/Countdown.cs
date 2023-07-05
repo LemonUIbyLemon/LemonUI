b26c7e2ffee15f4f8457ef3de7b902e816e5602e
Hannele Ruiz <justlemoncl@gmail.com>
2023-07-04 23:40:35 -0400

    Added base Countdown class

---
+++ LemonUI/Scaleform/Countdown.cs
@@ -0,0 +1,28 @@
+namespace LemonUI.Scaleform
+{
+    /// <summary>
+    /// The Countdown scaleform in the GTA Online races.
+    /// </summary>
+    public class Countdown : BaseScaleform
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new countdown scaleform.
+        /// </summary>
+        public Countdown() : base("COUNTDOWN")
+        {
+        }
+
+        #endregion
+
+        #region Functions
+
+        /// <inheritdoc/>
+        public override void Update()
+        {
+        }
+
+        #endregion
+    }
+}
