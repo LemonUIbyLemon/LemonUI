1f0208ffdb2f5681e7bf5301cd5bd8fe522beb66
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2023-11-26 18:37:25 -0800

    TinyTween

---
+++ LemonUI.SHVDN3/TinyTween/ScaleFunc.cs
@@ -0,0 +1,9 @@
+﻿namespace LemonUI.TinyTween
+{
+    /// <summary>
+    /// Represents a delegate for a scale function used in a tween.
+    /// </summary>
+    /// <param name="progress">The progress of the tween.</param>
+    /// <returns>The scaled value based on the progress.</returns>
+    public delegate float ScaleFunc(float progress);
+}
