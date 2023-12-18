c38f79ce21007bb731185837440a48a46db6fe63
Hannele Ruiz <justlemoncl@gmail.com>
2023-12-18 20:03:35 -0300

    Added basic class for ytd based animations

---
+++ LemonUI/Elements/ScaledAnim.cs
@@ -0,0 +1,45 @@
+using System.Drawing;
+
+namespace LemonUI.Elements
+{
+    /// <summary>
+    /// A scaled animation using YTD files with all of the frames.
+    /// </summary>
+    public class ScaledAnim : BaseElement
+    {
+        #region Properties
+
+        /// <summary>
+        /// The dictionary that contains the textures.
+        /// </summary>
+        public string Dict { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new dictionary based animation.
+        /// </summary>
+        /// <param name="dict">The texture dictionary (YTD) to use.</param>
+        /// <param name="pos">The position of the animation.</param>
+        /// <param name="size">The size of the animation.</param>
+        public ScaledAnim(string dict, PointF pos, SizeF size) : base(pos, size)
+        {
+            Dict = dict;
+        }
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// Draws the animation.
+        /// </summary>
+        public override void Draw()
+        {
+        }
+
+        #endregion
+    }
+}
